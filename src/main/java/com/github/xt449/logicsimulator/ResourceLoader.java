448d32a3d6a6cf62a3ea5015d9a1f60e802974b5
xt449 <alpha.xt449.omega@gmail.com>
2020-12-08 21:21:24 -0500

    Clean up

--- src/main/java/com/github/xt449/logicsimulator/ResourceLoader.java
+++ src/main/java/com/github/xt449/logicsimulator/ResourceLoader.java
@@ -1,12 +1,7 @@
 package com.github.xt449.logicsimulator;
 
-import org.lwjgl.BufferUtils;
-import org.lwjgl.stb.STBImage;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
 import java.util.stream.Collectors;
 
 /**
@@ -16,16 +11,5 @@ abstract class ResourceLoader {
 
 	static String readAllLines(String resourceLocation) {
 		return new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream(resourceLocation))).lines().collect(Collectors.joining("\n"));
-	}
-
-	static IntBuffer widthPointer;
-	static IntBuffer heightPointer;
-	static IntBuffer channels;
-
-	static ByteBuffer readBytesImage(String resourceLocation) {
-		widthPointer = BufferUtils.createIntBuffer(1);
-		heightPointer = BufferUtils.createIntBuffer(1);
-		channels = BufferUtils.createIntBuffer(1);
-		return STBImage.stbi_load(Main.class.getResource(resourceLocation).getPath().substring(1), widthPointer, heightPointer, channels, STBImage.STBI_rgb_alpha);
 	}
 }
